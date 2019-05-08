abe28724f512a923b2eef07bbeed12ef71b3b0d1
bemarlan <beverlymlanning@gmail.com>
2019-05-08 16:11:09 -0400

    Removes deprecated code in preparation for D9.

--- src/AnalyticsAuth0Manager.php
+++ src/AnalyticsAuth0Manager.php
@@ -1,24 +1,40 @@
 <?php
 
-namespace Drupal\analytics\Controller;
+namespace Drupal\analytics_auth0;
 
-use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\Core\Database\Connection;
 
 /**
  * Controller for Drupal user and Auth0 user.
  */
-class AnalyticsController extends ControllerBase {
+class AnalyticsAuth0Manager {
 
   /**
    * Current user.
+   *
+   * @var Drupal\Core\Session\AccountProxy
    */
   protected $user;
 
   /**
-   * Constructs the AnalyticsController.
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
    */
-  public function __construct() {
-    $this->user = \Drupal::currentUser();
+  protected $connection;
+
+  /**
+   * Constructs the AnalyticsAuth0Manager.
+   *
+   * @param Drupal\Core\Session\AccountProxy $account
+   *   Current account.
+   * @param \Drupal\Core\Database\Connection $connection
+   *   Established database connection.
+   */
+  public function __construct(AccountProxy $account, Connection $connection) {
+    $this->user = $account;
+    $this->connection = $connection;
   }
 
   /**
@@ -27,11 +43,11 @@ class AnalyticsController extends ControllerBase {
    * @param int $drupalId
    *   The Drupal user account ID.
    *
-   * @return arr
+   * @return mixed
    *   Auth0 user object.
    */
   private function findAuth0User($drupalId) {
-    $auth0User = db_select('auth0_user', 'a')
+    $auth0User = $this->connection->select('auth0_user', 'a')
       ->fields('a', ['auth0_id'])
       ->condition('drupal_id', $drupalId, '=')
       ->execute()
